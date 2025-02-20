43c528620337691aa854e2e65c997e60aae432e4
smrcorp007@gmail.com <smrcorp007@gmail.com>
2025-02-20 14:55:07 +0100

    Autre de tentative de resolution de Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 8080,
-    hot: true
-  }
+  // root: path.resolve(__dirname, 'src'),
+  // resolve: {
+  //   alias: {
+  //     '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+  //   }
+  // },
+  // build: {
+  //   outDir: 'dist',
+  // },
+  // server: {
+  //   port: 8080,
+  //   hot: true
+  // }
 
 })
